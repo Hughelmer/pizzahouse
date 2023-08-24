b082ad456635e92d26913ca4f33f4dea021dbe90
julimer.l.b <julimer.l.b@gmail.com>
2023-08-24 11:09:43 +0800

    Add POST for create pizza blade and redirect to welcome page with order confirmation message

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -28,7 +28,21 @@ class PizzaController extends Controller
   }
 
   public function create() {
-    return view('pizzas.create')
-;  }
+    return view('pizzas.create');
+  }
+
+  public function store() {
+
+    $pizza = new Pizza();
+
+    $pizza->name = request('name');
+    $pizza->type = request('type');
+    $pizza->base = request('base');
+
+   $pizza->save();
+
+    return redirect('/')->with('mssg', 'Order successfully!');
+
+  }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,6 @@ Route::get('/', function () {
 //Pizza Routes
 Route::get('/pizzas', [PizzaController::class, 'index']);
 Route::get('/pizzas/create', [PizzaController::class, 'create']);
+Route::post('/pizzas', [PizzaController::class, 'store']);
 Route::get('/pizzas/{id}', [PizzaController::class, 'show']);
 
